b3b4b15f1528707aff57217565303c629efbfd45
tk <tk at ibotic net>
2015-02-24 20:10:17 +0100

    cleanup

--- lib/capistrano-drupal.rb
+++ lib/capistrano-drupal.rb
@@ -1,16 +1,3 @@
-#   before "deploy",
-#         "drush:site_offline",
-#         "drush:backupdb"
-#   after "deploy:update_code",
-#         "drupal:stage_settings",
-#         "drupal:stage_htaccess",
-#         "drupal:symlink_shared"
-#   after "deploy",
-#         "drush:updatedb",
-#         "drush:cache_clear",
-#         "drush:site_online"
-#   after "deploy", "git:push_deploy_tag"
-#
 load File.expand_path('../capistrano/tasks/deploy.rake', __FILE__)
 load File.expand_path('../capistrano/tasks/drupal.rake', __FILE__)
 load File.expand_path('../capistrano/tasks/drush.rake', __FILE__)
--- lib/capistrano/drupal/defaults.rb
+++ lib/capistrano/drupal/defaults.rb
@@ -7,8 +7,7 @@
 # defaults:
 # set   :shared_path,   "#{:deploy_to}/shared"
 # set   :releases_path, "#{:deploy_to}/releases"
-#
-# set   :app_before_deploy  "#{fetch(:releases_path)}"
+
 set   :app_path,      "app"
 set   :site_path,     "#{fetch(:app_path)}/sites/default"
 
@@ -16,8 +15,6 @@ set   :drush_cmd,     "drush"
 set   :backup_dest,   "manual"
 
 set :site_dirs,     ['files', 'tmp', 'private']
-#   set :site_files,    ['settings.php']
-set :shared_dirs,   ['files', 'tmp', 'private']
 
 # Dirs that need to remain the same between deploys (shared dirs)
 # set :linked_dirs,   ["#{fetch(:app_path)}/sites/default/files", "#{fetch(:app_path)}/sites/default/tmp", "#{fetch(:app_path)}/sites/default/private"]
